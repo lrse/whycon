56277a3f799a03ed0effca1b62a0c4ffd5a960a5
v01d <phreakuencies@gmail.com>
2014-05-31 17:34:20 -0300

    remove mavlink/mavconn/pcl support

--- config.h.cmake
+++ config.h.cmake
@@ -1,15 +1,6 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#cmakedefine PCL_FOUND
-#cmakedefine ENABLE_3D
-
-#if defined(PCL_FOUND) && defined(ENABLE_3D)
-#define ENABLE_VIEWER
-#endif
-
-#cmakedefine ENABLE_MAVCONN
-
 #cmakedefine ENABLE_FULL_UNDISTORT
 #cmakedefine ENABLE_RANDOMIZED_THRESHOLD
 
